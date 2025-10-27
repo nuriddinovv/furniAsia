d91f513592e16b4f450218e4dfb03fd218102067
masharipov <masharipovfarrux@gmail.com>
2025-10-27 10:37:48 +0500

    android errors

--- android/app/src/main/java/com/tempandroid/MainActivity.kt
+++ android/app/src/main/java/com/tempandroid/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.furniasia
+package com.tempandroid
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "furniAsia"
+  override fun getMainComponentName(): String = "TempAndroid"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
